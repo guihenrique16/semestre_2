92314733855cea36288f6524b604dee0d45ef648
Guilherme Henrique Garbelini Alberto <46561309820@info.local>
2023-08-17 16:45:42 -0300

    criados

---
+++ Projeto-healthClinic/Desafios/Desafio_1.0.sql
@@ -0,0 +1,3 @@
+-- DESAFIO1
+-- Criar função para retornar os médicos de uma determinada especialidade
+
---
+++ Projeto-healthClinic/Desafios/Desafio_1.1.sql
@@ -0,0 +1,3 @@
+-- DESAFIO2
+-- Criar procedure para retornar a idade de um determinado usuário específico
+
