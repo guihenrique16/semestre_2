6e7623ea5d168792a382f6521aebcbe125f814b9
Guilherme Henrique Garbelini Alberto <46561309820@info.local>
2023-08-18 14:43:44 -0300

    criados

--- Projeto-healthClinic/Desafios/Desafio_1.0.sql
+++ Projeto-healthClinic/Desafios/Desafio_1.0.sql
@@ -1,3 +1,23 @@
--- DESAFIO1
--- Criar função para retornar os médicos de uma determinada especialidade
+---- DESAFIO1
+---- Criar função para retornar os médicos de uma determinada especialidade
 
+---- Criar uma função para retornar médicos por especialidade
+
+--CREATE FUNCTION MedicosPorEspecialidade
+--(
+--	@especialidade varchar(100)
+--)
+--RETURNS TABLE
+--AS
+--RETURN
+--(
+--    select MedicoUsuario.Nome as Médico, 
+--	Especialidades.TituloEspecialidades as Especialidade
+--	from Especialidades
+--	inner join Medico on Medico.IdEspecialidades = Especialidades.IdEspecialidades
+--	inner join Usuario as MedicoUsuario on Medico.IdUsuario = MedicoUsuario.IdUsuario
+--    WHERE  Especialidades.TituloEspecialidades = @especialidade 
+ 
+--);
+
+--SELECT * FROM MedicosPorEspecialidade('Neurologista');
